f69398b3719a1697256a21b2d1f7e100316b5cdd
Min ho Kim <minho42@gmail.com>
2020-12-31 11:24:53 +1100

    fix typos

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+    ],
     keywords="misspelling, grammar",
     package_dir={"": "typofinder"},
     packages=find_packages(where="typofinder"),
